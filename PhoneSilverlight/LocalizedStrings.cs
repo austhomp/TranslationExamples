7d2ef108e60bc6c27ff72fc66b62cb737432d388
Austin Thompson <austhomp@users.noreply.github.com>
2014-10-02 18:27:42 -0500

    Added more projects

---
+++ PhoneSilverlight/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PhoneSilverlight.Resources;
+
+namespace PhoneSilverlight
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
